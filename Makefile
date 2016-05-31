8718272bf0b8c004bc746669ac284bf4a91fc7d5
ReeceStevens <reecestevens@utexas.edu>
2016-05-31 12:01:50 -0500

    Improve JSON output, makefile, and doc URL capture
    Improve the readability of json files. Also, parse the documentation
    URL links for each reference in the CIOD-module tables.
    
    Encapsulate the raw CIOD-module parsing into a function and
    perform the file IO inside the script instead of in the Makefile.
    
    Also, add an install rule to make setup easier.

--- Makefile
+++ Makefile
@@ -2,8 +2,12 @@ all: parser.py PS3.3.html
 	cp PS3.3.html temp_standard.html
 	sed -i -e 's/&nbps;/ /g' temp_standard.html
 	rm -f temp_standard.html-e
-	python parser.py > ciod_modules.json
+	python parser.py
 	rm -f temp_standard.html
 
 clean: 
-	rm -f ciod_modules.json 
+	rm -f ciod_module_rough.json parser.pyc requirements.txt
+
+install:
+	pip freeze > requirements.txt
+	pip install -r requirements.txt
--- parser.py
+++ parser.py
@@ -11,17 +11,20 @@ import re
 def main():
     html_doc = open('temp_standard.html', 'r')
     standard = BeautifulSoup(html_doc, 'html.parser')
-    
-    iod_table_pattern = re.compile(".*IOD Modules$")
+    get_ciod_module_raw(standard)
 
+def get_ciod_module_raw(standard):
+    iod_table_pattern = re.compile(".*IOD Modules$")
     ciod_table_divs = get_composite_IOD_divs(standard)
     # Extract all the composite IOD tables 
+    ciod_module_rough = open('ciod_module_rough.json', 'w')
     for tdiv in ciod_table_divs:
         data = []
         table_name = tdiv.p.strong.get_text()
         if iod_table_pattern.match(table_name):
             table_body = tdiv.div.table.tbody
             table_data = extract_table_data(table_body)
+            urls = extract_doc_links(table_body)
             last_ie = table_data[0][0]
             ies = []
             modules = []
@@ -45,9 +48,18 @@ def main():
                     i += 1
                     usage.append(row[i])
                 except IndexError:
-                    print("Index error, table row not conforming to standard IOD table structure.")
-            print json.dumps([table_name, {'IE': unicode(ies),'Module': unicode(modules), 'Reference': unicode(references), 'Usage': unicode(usage)} ], sort_keys=True, indent=4, separators=(',',':'))
-            
+                    ciod_module_rough.write("Index error, table row not conforming to standard IOD table structure.\n")
+            ciod_module_rough.write(json.dumps([table_name, {'IE': ies,'Module': modules, 'Reference': references, 'Usage': usage, 'Urls': urls} ], sort_keys=True, indent=4, separators=(',',':')) + "\n")
+    ciod_module_rough.close()
+
+def extract_doc_links(table_body):
+    data = []
+    links = table_body.find_all('a')
+    for link in links:
+        if (link.get('href')):
+            data.append(link.get('href'))
+    return data
+
 def extract_table_data(table_body):
     data = []
     rows = table_body.find_all('tr')
