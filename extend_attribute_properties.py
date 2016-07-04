429afacca49e4d522d0eeaf2e200d6c4ecbbd8f9
J. David Giese <johndgiese@gmail.com>
2016-07-03 23:43:52 -0400

    Clean up `extend_attributes_properties.py`
    - Improve naming; some of the names were innaccurate or vauge, e.g.
    `properties` is vague, and `standard_attrs` seems to indicate that it is
    a list of attributes, when it is a list of modules that contain
    attributes.
    - Add a docstring with some context
    - The `natural_join` function was doing two things:
        1. extracting the `data` key
        2. actually doing the join
      pulled out job 1 to a higher level of abstraction

--- extend_attribute_properties.py
+++ extend_attribute_properties.py
@@ -9,30 +9,31 @@ import pandas as pd
 from parse_lib import dump_pretty_json, read_json_to_dict
 
 
-def join_by_tag_attrs_and_properties(standard_attrs, properties):
-    full_attrs = []
-    properties_dataframe = pd.DataFrame.from_dict(properties, orient='index')
-    properties_dataframe.index.name = 'tag'
-    for module in standard_attrs:
-        new_module = natural_join(properties_dataframe, module)
-        full_attrs.append(new_module)
-    return full_attrs
+def extend_module_attributes(modules_with_attributes, data_dictionary):
+    '''
+    Given a list of DICOM modules each containing a list of DICOM attributes
+    from section 3, extend them with extra properties from the DICOM data
+    dictionary in section 6.
+    '''
+    data_dictionary_dataframe = pd.DataFrame.from_dict(data_dictionary, orient='index')
+    data_dictionary_dataframe.index.name = 'tag'
+    for module in modules_with_attributes:
+        module_attributes = module['data']
+        extended_module_attributes = natural_join(data_dictionary_dataframe, module_attributes)
+        module['data'] = extended_module_attributes
+    return modules_with_attributes
 
 
-def natural_join(properties_dataframe, module):
-    new_module = {'name': module['name'], 'slug': module['slug'],
-                  'link_to_standard': module['link_to_standard'], 'data': []}
-    module_dataframe = pd.DataFrame(module['data'])
-    joined_dataframe = pd.merge(module_dataframe, properties_dataframe,
-                                left_on='tag', right_index=True)
-    new_module['data'] = (joined_dataframe.to_dict(orient='records'))
-    return new_module
+def natural_join(data_dictionary_dataframe, raw_attributes):
+    raw_attributes_dataframe = pd.DataFrame(raw_attributes)
+    joined_dataframe = pd.merge(module_dataframe, raw_attributes_dataframe, left_on='tag', right_index=True)
+    return joined_dataframe.to_dict(orient='records')
 
 
 if __name__ == '__main__':
-    standard_attrs = read_json_to_dict(sys.argv[1])
-    properties = read_json_to_dict(sys.argv[2])
+    modules_with_attributes = read_json_to_dict(sys.argv[1])
+    data_dictionary = read_json_to_dict(sys.argv[2])
 
-    full_attrs = join_by_tag_attrs_and_properties(standard_attrs, properties)
+    modules_with_extended_attributes = extend_module_attributes(modules_with_attributes, data_dictionary)
 
-    dump_pretty_json(sys.argv[3], 'w', full_attrs)
+    dump_pretty_json(sys.argv[3], 'w', modules_with_extended_attributes)
