2988ff25cdc6805b27d99c1170ad43f1acf7ff3c
ReeceStevens <reecestevens24@gmail.com>
2016-09-14 22:41:25 -0500

    Extend unit tests covering extra parsing sections

--- tests/extra_sections_parse_test.py
+++ tests/extra_sections_parse_test.py
@@ -9,6 +9,14 @@ example_figure_html = '''<div><a id="figure_link"></a><div><div><img src="figure
 
 corrected_figure_html = '''<div><a id="figure_link"></a><div><div><img src="http://dicom.nema.org/medical/dicom/current/output/html/figures/somefig.svg"/></div></div></div>'''
 
+expandable_html = '''<div><a href="#something"></a></div>'''
+expanded_html = '''<div><a href="http://dicom.nema.org/medical/dicom/current/output/html/part03.html#something"></a></div>'''
+
+expandable_external_ref_html = '''<div><a href="part16.html#something"></a></div>'''
+expanded_external_ref_html = '''<div><a href="http://dicom.nema.org/medical/dicom/current/output/html/part16.html#something"></a></div>'''
+
+big_html_block = '<div>' + example_section_html + '</div>'
+
 def test_get_section_html():
     parseable_html = BeautifulSoup(example_section_html, 'html.parser')
     ref_section = p.html_string_from_reference('part03.html#sect_link', parseable_html)
@@ -17,4 +25,10 @@ def test_get_section_html():
 def test_get_figure_html():
     parseable_html = BeautifulSoup(example_figure_html, 'html.parser')
     ref_section = p.html_string_from_reference('part03.html#figure_link', parseable_html)
-    assert ref_section == corrected_figure_html 
+    assert ref_section == corrected_figure_html
+
+def test_expand_hrefs_part03():
+    assert expanded_html == p.expand_hrefs_to_absolute(expandable_html)
+
+def test_expand_hrefs_external():
+    assert expanded_external_ref_html == p.expand_hrefs_to_absolute(expandable_external_ref_html)
