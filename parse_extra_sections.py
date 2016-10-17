8661717c403f2b3f5c7b8ac81c41e2d57a00588e
J. David Giese <johndgiese@gmail.com>
2016-10-17 00:02:01 -0400

    Whitespace + PEP8
    - Add double newlines
    - Fix inconsitent tabbing
    - Put minimal number of lines in `with` statement for readability

--- parse_extra_sections.py
+++ parse_extra_sections.py
@@ -1,10 +1,13 @@
 import sys
 import re
+
 from bs4 import BeautifulSoup, NavigableString
 
 import parse_lib as pl
 
+
 BASE_URL = "http://dicom.nema.org/medical/dicom/current/output/html/"
+
 
 def parse_extra_standard_content(module_to_attributes, parseable_html):
     extra_sections = {}
@@ -13,6 +16,7 @@ def parse_extra_standard_content(module_to_attributes, parseable_html):
         attribute['description'] = updated_description
         extra_sections = {**extra_sections, **referenced_sections}
     return extra_sections, module_to_attributes
+
 
 def get_all_references(attribute_description, parseable_html, extra_sections):
     description_html = BeautifulSoup(attribute_description, 'html.parser')
@@ -33,13 +37,15 @@ def get_all_references(attribute_description, parseable_html, extra_sections):
                     mark_as_saved(anchor)
     return sections, str(description_html)
 
+
 def clean_html_string(html_string):
     parseable_html = BeautifulSoup(html_string, 'html.parser')
     parent_div = parseable_html.find('div')
     for tag in parent_div.descendants:
-            tag = remove_attributes(tag)
+        tag = remove_attributes(tag)
     parent_div = remove_attributes(parent_div)
     return str(parent_div)
+
 
 def remove_attributes(tag):
     allowed_attributes = ["class", "href", "src", "type", "data"]
@@ -47,9 +53,11 @@ def remove_attributes(tag):
         tag.attrs = {k: v for k,v in tag.attrs.items() if k in allowed_attributes}
     return tag
 
+
 def mark_as_saved(anchor):
     anchor['href'] = ''
     anchor.name = 'span'
+
 
 def html_string_from_reference(target_section, parseable_html):
     if '#' not in target_section:
@@ -70,6 +78,7 @@ def html_string_from_reference(target_section, parseable_html):
         elif re.match('figure.*', section_id) is not None:
             referenced_html = get_figure_html(id_tag)
         return referenced_html
+
 
 def expand_resource_links_to_absolute(raw_html):
     html = BeautifulSoup(raw_html, 'html.parser')
@@ -95,16 +104,21 @@ def expand_resource_links_to_absolute(raw_html):
 def get_section_html(id_tag):
     return str(id_tag.parent.parent.parent.parent.parent)
 
+
 def get_figure_html(id_tag):
     top_div = id_tag.parent
     img_tag = top_div.div.div.img
     img_tag['src'] = BASE_URL + img_tag['src']
     return str(id_tag.parent)
 
+
 if __name__ == "__main__":
+    module_to_attributes = pl.read_json_to_dict(sys.argv[2])
+
     with open(sys.argv[3], 'r') as standard_html:
-        module_to_attributes = pl.read_json_to_dict(sys.argv[2])
         parseable_html = BeautifulSoup(standard_html, 'html.parser')
-        extra_sections, updated_module_attributes = parse_extra_standard_content(module_to_attributes, parseable_html)
-        pl.write_pretty_json(sys.argv[1], extra_sections)
-        pl.write_pretty_json(sys.argv[2], updated_module_attributes)
+
+    extra_sections, updated_module_attributes = parse_extra_standard_content(module_to_attributes, parseable_html)
+
+    pl.write_pretty_json(sys.argv[1], extra_sections)
+    pl.write_pretty_json(sys.argv[2], updated_module_attributes)
