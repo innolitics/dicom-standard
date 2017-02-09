e8713adf29681d53c462251ef693396f6f84a5d6
ReeceStevens <reecestevens24@gmail.com>
2017-02-08 21:39:31 -0600

    Rename some long constants and refactor an old fcn

--- extract_ciod_module_data.py
+++ extract_ciod_module_data.py
@@ -10,24 +10,20 @@ import parse_lib as pl
 import parse_relations as pr
 from table_utils import expand_spans, table_to_dict, stringify_table, tdiv_to_table_list
 
-CIOD_CHAPTER_ID = 'chapter_A'
-CIOD_TABLE_SUFFIX = re.compile(".*IOD Modules$")
-CIOD_COLUMN_TITLES = ['informationEntity', 'module', 'reference_fragment', 'usage']
+CHAPTER_ID = 'chapter_A'
+TABLE_SUFFIX = re.compile(".*IOD Modules$")
+COLUMN_TITLES = ['informationEntity', 'module', 'reference_fragment', 'usage']
 
 URL_PREFIX = "http://dicom.nema.org/medical/dicom/current/output/html/part03.html#"
 
 def get_ciod_tables(standard):
-    chapter_A_table_divs = pl.all_tdivs_in_chapter(standard, CIOD_CHAPTER_ID)
+    chapter_A_table_divs = pl.all_tdivs_in_chapter(standard, CHAPTER_ID)
     ciod_table_divs = list(filter(is_valid_ciod_table, chapter_A_table_divs))
     ciod_table_lists = list(map(tdiv_to_table_list, ciod_table_divs))
     return (ciod_table_lists, ciod_table_divs)
 
 def is_valid_ciod_table(table_div):
-    return CIOD_TABLE_SUFFIX.match(pr.table_name(table_div))
-
-def tr_to_row_list(tr):
-    cells = tr.find_all('td')
-    return cells
+    return TABLE_SUFFIX.match(pr.table_name(table_div))
 
 
 def tables_to_json(tables, tdivs):
@@ -37,7 +33,7 @@ def tables_to_json(tables, tdivs):
     return list(map(get_table_with_metadata, zip(table_dicts, tdivs)))
 
 def ciod_table_to_dict(table):
-    return table_to_dict(table, CIOD_COLUMN_TITLES)
+    return table_to_dict(table, COLUMN_TITLES)
 
 def get_table_with_metadata(table_with_tdiv):
     table, tdiv = table_with_tdiv
