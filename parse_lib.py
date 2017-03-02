74326ae9fe5c95b9187a3e791193fa7e01a48871
ReeceStevens <reecestevens24@gmail.com>
2017-03-01 23:58:37 -0600

    Delete old stages and clean parse_lib
    Deleted the old parsing stages that have been redesigned and replaced.
    In the process, a lot of old functions from `parse_lib.py` were able to
    be deleted. There is still more deleting to be had after the refactor is
    complete, but this is to clear space so all the files don't become
    confusing.

--- parse_lib.py
+++ parse_lib.py
@@ -5,48 +5,14 @@ DICOM standard HTML file.
 
 import json
 import re
-from copy import deepcopy
 from bs4.element import Tag
 
-from bs4 import BeautifulSoup, NavigableString
-
+from bs4 import BeautifulSoup
 
 FULL_TABLE_COLUMN_NUM = 4
 REFERENCE_COLUMN = 2
 
 BASE_DICOM_URL = "http://dicom.nema.org/medical/dicom/current/output/html/"
-
-def table_data_from_standard(standard, chapter_name, match_pattern, column_titles, column_correction):
-    '''
-    Given a section of the standard, parse the HTML tables and return the data
-    in a JSON file.
-    '''
-    all_tables = standard.find_all('div', class_='table')
-    chapter_tables = all_tdivs_in_chapter(standard, chapter_name)
-    all_table_dicts = []
-    for tdiv in chapter_tables:
-        table_name = tdiv.p.strong.get_text()
-        table_id = tdiv.a.get('id')
-        if match_pattern.match(table_name):
-            final_table_as_list_of_lists = condition_table_data(tdiv, all_tables, column_correction)
-            final_table_as_list_of_dicts = table_to_dict(final_table_as_list_of_lists, column_titles)
-
-            clean_name = clean_table_name(table_name)
-            slug = create_slug(clean_name)
-            doc_link = standard_link_from_fragment(table_id)
-            description = get_table_description(tdiv)
-
-            table_dict = {
-                'name': clean_name,
-                'data': final_table_as_list_of_dicts,
-                'id': slug,
-                'description': description,
-                'linkToStandard': doc_link
-            }
-
-            all_table_dicts.append(table_dict)
-
-    return all_table_dicts
 
 def all_tdivs_in_chapter(standard, chapter_name):
     chapter_divs = standard.find_all('div', class_='chapter')
@@ -55,75 +21,9 @@ def all_tdivs_in_chapter(standard, chapter_name):
             table_divs = chapter.find_all('div', class_='table')
             return table_divs
 
-
-def clean_table_entry(name):
-    clean_title, *splits = re.split('Module', name)
-    return clean_title.strip()
-
-
 def create_slug(title):
     first_pass = re.sub(r'[\s/]+', '-', title.lower())
     return re.sub(r'[\(\),\']+', '', first_pass)
-
-
-def condition_table_data(tdiv, all_tables, column_correction):
-    # TODO: pull out `column_correction` one-off
-    # This is very specific to parsing a specific type of function, and should
-    # not be in our general purpose "parse lib"
-
-    raw_table = table_to_list(tdiv, all_tables)
-
-    full_table = expand_spans(raw_table)
-
-    if column_correction:
-        full_table = correct_for_missing_type_column(full_table)
-    link_correction = not column_correction
-    unstripped_text_table = extract_text_from_html(full_table, link_correction)
-    text_table = [map(strip_whitespace, row) for row in unstripped_text_table]
-    return text_table
-
-
-def extract_text_from_html(full_table, link_correction):
-    final_table = []
-    for row in full_table:
-        temp_row = []
-        for idx, cell in enumerate(row):
-            description_cell = idx == 3
-            temp_row.append(text_or_href_from_cell(cell, idx, link_correction, description_cell))
-        final_table.append(temp_row)
-    return final_table
-
-
-def extraneous_attribute_table_row(row):
-    # TODO: pull this out from the general parsing lib
-    cells = row.find_all('td')
-    expected_number_of_cells = 3
-    return len(cells) < expected_number_of_cells
-
-
-def strip_whitespace(attribute_value):
-    if isinstance(attribute_value, str):
-        return attribute_value.strip()
-    else:
-        return attribute_value
-
-
-def correct_for_missing_type_column(full_table):
-    '''
-    Corrects for the attribute tables which sometimes do not have the type
-    column in the HTML version of the standard.
-    '''
-    corrected_table = []
-    for a_name, a_tag, a_type, a_descr, a_macro in full_table:
-        corrected_row = [a_name, a_tag]
-        if a_descr is None:
-            a_corrected_type = a_descr
-            a_corrected_descr = a_type
-            corrected_row.extend([a_corrected_type, a_corrected_descr, a_macro])
-        else:
-            corrected_row.extend([a_type, a_descr, a_macro])
-        corrected_table.append(corrected_row)
-    return corrected_table
 
 
 def find_spans(table):
@@ -135,7 +35,7 @@ def find_spans(table):
     for row in table:
         row_spans = []
         for column, cell in enumerate(row):
-            description_cell = column ==  3
+            description_cell = column == 3
             cell_span = span_from_cell(cell, description_cell)
             row_spans.append(cell_span)
         spans.append(row_spans)
@@ -152,7 +52,7 @@ def span_from_cell(cell, description_cell):
             return [
                 int(td_tag.get('rowspan', 1)),
                 int(td_tag.get('colspan', 1)),
-                getRawInnerHTML(td_tag)
+                get_raw_inner_html(td_tag)
             ]
         else:
             return [
@@ -163,150 +63,12 @@ def span_from_cell(cell, description_cell):
     except AttributeError:
         return [1, 1, cell]
 
-def getRawInnerHTML(tag):
+def get_raw_inner_html(tag):
     return ''.join(list(map(str, tag.contents)))
 
 def td_html_content(td_html):
     split_html = re.split('(<td.*?>)|(</td>)', td_html)
     return split_html[3]
-
-
-def expand_spans(table):
-    '''
-    Fills in tables by unpacking rowspans and colspans. Results in a
-    table with an equal number of cells in each row.
-    '''
-    expanded_table = []
-    spans = find_spans(table)
-    for i in range(len(table)):
-        row = []
-        for j in range(len(table[i])):
-            spans, html = expand_span_in_cell(spans, i, j)
-            row.append(html)
-        expanded_table.append(row)
-    return expanded_table
-
-
-def expand_span_in_cell(spans, i, j):
-    if spans[i][j] is None:
-        return spans, None
-    else:
-        rowspan, colspan, html = spans[i][j]
-        if rowspan > 1:
-            spans = expand_rowspan(spans, i, j)
-        if colspan > 1:
-            spans = expand_colspan(spans, i, j)
-        return spans, html
-
-
-def expand_rowspan(spans, i, j):
-    row_slides = spans[i][j][0] - 1
-    spans[i][j][0] = 1
-    for k in range(1, row_slides+1):
-        spans[i+k] = slide_down(j-1, 1, spans[i+k])
-        spans[i+k][j] = deepcopy(spans[i][j])
-    return spans
-
-
-def expand_colspan(spans, i, j):
-    col_slides = spans[i][j][1] - 1
-    spans[i][j][1] = 1
-    spans[i] = slide_down(j, col_slides, spans[i])
-    for k in range(1, col_slides+1):
-        spans[i][j+k] = deepcopy(spans[i][j])
-    return spans
-
-
-def slide_down(start_idx, num_slides, row):
-    '''
-    Moves cells down a row or column by num_slides positions starting
-    after index start_idx. Used to make room for rowspan and colspan
-    unpacking.
-    '''
-    try:
-        sliding_columns = row[start_idx+1:len(row)-num_slides]
-        new_row = row[0:len(row)-len(sliding_columns)]
-        new_row.extend(sliding_columns)
-        return new_row
-    except IndexError:
-        raise ValueError('Cell spans beyond table!')
-
-
-def table_to_list(table_div, macro_table_list=None, macro_table_id=None):
-    '''
-    Converts an HTML table to a 2D list, expanding macros along the way.
-    '''
-    # TODO: split this into smaller parts
-    if table_div is None:
-        return None
-    current_table_id = table_div.a.get('id')
-    table = []
-    table_body = table_div.find('tbody')
-    for row in table_body.find_all('tr'):
-        macro_reference = check_for_macros(row, macro_table_list, current_table_id)
-        if macro_reference is not None:
-            table.extend(macro_reference)
-            continue
-        first_cell = row.find('td')
-        exception_rows = re.compile("(.*COD.*)|(.*Include.*)|(.*Any Attribute.*)", re.DOTALL)
-        if exception_rows.match(first_cell.get_text()):
-            continue
-        cells = convert_row_to_list(row, macro_table_id)
-        table.append(cells)
-    return table
-
-
-def convert_row_to_list(row, macro_table_id):
-    cells = []
-    all_cells_in_row = row.find_all('td')
-    for cell in all_cells_in_row:
-        cells.append(str(cell))
-    for j in range(len(cells), 4):
-        cells.append(None)
-    cells.append(macro_table_id)
-    return cells
-
-
-def check_for_macros(row, macro_table_list, current_table_id):
-    if macro_table_list is not None:
-        all_cells_in_row = row.find_all('td')
-        cell = all_cells_in_row[0]
-        specified_macro = None
-        if is_macro_link(cell):
-            specified_macro = macro_expansion(cell, current_table_id, macro_table_list)
-        if specified_macro is not None:
-            return specified_macro
-    return None
-
-
-def is_macro_link(cell):
-    link_pattern = re.compile('.*Include.*')
-    try:
-        has_link = cell.p.span.a is not None
-        link_is_include = link_pattern.match(cell.p.span.get_text()) is not None
-        return has_link and link_is_include
-    except AttributeError:
-        return False
-
-
-def macro_expansion(cell, current_table_id, macro_table_list):
-    if is_macro_link(cell):
-        table_id = extract_referenced_table_id(cell)
-        if table_id == current_table_id:
-            return None
-        macro_div = find_table_div(macro_table_list, table_id)
-        macro_table = table_to_list(macro_div, macro_table_list, table_id)
-        prepend_sequence_indicators(cell, macro_table)
-        return macro_table
-    return None
-
-
-def extract_referenced_table_id(cell):
-    link = cell.p.span.a.get('href')
-    _url, _pound, table_id = link.partition('#')
-    if table_id is None:
-        raise ValueError("Formatting error")
-    return table_id
 
 
 def find_table_div(all_tables, table_id):
@@ -319,47 +81,11 @@ def find_table_div(all_tables, table_id):
         return None
 
 
-def prepend_sequence_indicators(cell, macro_table):
-    if macro_table is None:
-        return
-    indicator = sequence_indicator_from_cell(cell.p.span.get_text())
-    for row in macro_table:
-        row[0] = insert_indicator_into_html(indicator, row[0])
-
-
-def insert_indicator_into_html(indicator, html):
-    name_cell = BeautifulSoup(html, 'html.parser').find('td')
-    name_cell.insert(0, NavigableString(indicator))
-    return str(name_cell)
-
-
-def sequence_indicator_from_cell(cell):
-    preceding_space, *split = re.split('^(>+)', cell)
-    if split == []:
-        indicator = ''
-    else:
-        indicator = split[0]
-    return indicator
-
-
-def text_or_href_from_cell(cell_html, column_idx, link_correction, description_cell):
-    if cell_html is None:
-        return None
-    html = BeautifulSoup(cell_html, 'html.parser')
-    if column_idx == REFERENCE_COLUMN and link_correction:
-        anchor_tags = html.find_all('a')
-        return anchor_tags[-1].get('href')
-    elif description_cell and not link_correction:
-        return str(html)
-    else:
-        return clean_table_entry(html.get_text())
-
-
-
 def clean_table_name(name):
-    table, section, title = re.split('\u00a0', name)
-    clean_title, *splits = re.split('(IOD Modules)|(Module Attributes)|(Macro Attributes)|(Module Table)', title)
+    _, _, title = re.split('\u00a0', name)
+    clean_title, *_ = re.split(r'(IOD Modules)|(Module Attributes)|(Macro Attributes)|(Module Table)', title)
     return clean_title.strip()
+
 
 def clean_description(description_html):
     parsed_html = BeautifulSoup(description_html, 'html.parser')
@@ -371,8 +97,9 @@ def clean_description(description_html):
 def get_top_level_tag(parsed_html):
     top_level_tag = parsed_html.find('p', recursive=False)
     if top_level_tag is None:
-        top_level_tag = description_html.find('div', recursive=False)
+        top_level_tag = parsed_html.find('div', recursive=False)
     return top_level_tag
+
 
 def standard_link_from_fragment(fragment):
     url_prefix = "http://dicom.nema.org/medical/dicom/current/output/html/part03.html#"
@@ -397,6 +124,7 @@ def read_json_to_dict(filepath):
         json_dict = json.loads(json_string)
         return json_dict
 
+
 def remove_attributes_from_description_html(top_level_tag):
     top_level_tag.attrs = clean_tag_attributes(top_level_tag)
     for child in top_level_tag.descendants:
@@ -408,9 +136,10 @@ def clean_tag_attributes(tag, ignored_attributes=None):
     if ignored_attributes is None:
         ignored_attributes = ['href']
     if tag.attrs != {}:
-        return {a:v for a,v in tag.attrs.items() if a in ignored_attributes}
+        return {a: v for a, v in tag.attrs.items() if a in ignored_attributes}
     else:
         return tag.attrs
+
 
 def resolve_hrefs(tag, base_url):
     anchors = tag.find_all('a')
@@ -423,12 +152,14 @@ def resolve_hrefs(tag, base_url):
                 anchor['href'] = base_url + anchor['href']
     return tag
 
+
 def add_targets_to_anchors(tag):
     anchors = tag.find_all('a')
     for anchor in anchors:
         if 'href' in anchor.attrs.keys():
             anchor['target'] = '_blank'
     return tag
+
 
 def text_from_html_string(html_string):
     parsed_html = BeautifulSoup(html_string, 'html.parser')
