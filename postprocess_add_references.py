9317f16186e5d31048b4a4384ec8a9fe28757951
J. David Giese <johndgiese@gmail.com>
2017-03-12 22:40:20 -0400

    Remove unnecessary reference util module
    The references_utils module was only used by postprocess_add_references,
    hence it seemed like it was not necessary to split it out into a
    separate module.  Placing the code in a separate module just made the
    code more difficult to read, because you have to move over to the other
    file to look at the code.  Also, it is misleading, because the fact that
    it is in a separate file implies it is used elsewhere.

--- postprocess_add_references.py
+++ postprocess_add_references.py
@@ -5,16 +5,76 @@ add its source URL to the JSON file and record the referenced HTML in the
 on reference links that are successfully recorded.
 '''
 import sys
+import re
+from functools import partial
+
+from bs4 import BeautifulSoup
 
 import parse_lib as pl
 from macro_utils import flatten_one_layer
-from references_utils import (resolve_relative_resource_urls, references_in_module_attr_pair,
-                              get_source_url_from_section_id)
+
+IGNORED_REFERENCES_RE = re.compile(r'(.*ftp.*)|(.*http.*)|(.*part05.*)|(.*chapter.*)|(.*PS3.*)|(.*DCM.*)|(.*glossentry.*)')
+
+
+def references_in_module_attr_pair(pair):
+    references = get_valid_reference_anchors(pair['description'])
+    reference_locations = list(map(get_ref_standard_location, references))
+    reference_urls = list(map(get_source_url_from_section_id, reference_locations))
+    list(map(mark_as_recorded, references))
+    pair['externalReferences'] = [] if len(reference_urls) < 1 else reference_urls
+    return reference_locations
+
+
+def get_valid_reference_anchors(html):
+    anchor_tags = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
+    return [a for a in anchor_tags if not re.match(IGNORED_REFERENCES_RE, a['href'])]
+
+
+def get_source_url_from_section_id(standard_location):
+    return pl.BASE_DICOM_URL + '#'.join(standard_location)
+
+
+def get_ref_standard_location(reference_anchor_tag):
+    relative_link = reference_anchor_tag.get('href')
+    standard_page, section_id = relative_link.split('#')
+    standard_page = 'part03.html' if standard_page == '' else standard_page
+    return standard_page, section_id
+
+
+def resolve_relative_resource_urls(html_string):
+    html = BeautifulSoup(html_string, 'html.parser')
+    anchors = html.find_all('a', href=True)
+    imgs = html.find_all("img", src=True)
+    equations = html.find_all("object", data=True)
+    list(map(resolve_anchor_href, anchors))
+    list(map(partial(resolve_resource, 'src'), imgs))
+    list(map(partial(resolve_resource, 'data'), equations))
+    return str(html)
+
+
+def resolve_anchor_href(anchor):
+    if not has_protocol_prefix(anchor):
+        anchor['href'] = pl.BASE_DICOM_URL + anchor['href']
+
+
+def has_protocol_prefix(anchor):
+    return re.match(r'(http)|(ftp)', anchor['href'])
+
+
+def resolve_resource(url_attribute, resource):
+    resource[url_attribute] = pl.BASE_DICOM_URL + resource[url_attribute]
+
+
+def mark_as_recorded(anchor):
+    anchor['href'] = ''
+    anchor.name = 'span'
+
 
 def add_refs_to_pairs(module_attr_pairs):
     reference_locations = [references_in_module_attr_pair(pair) for pair in module_attr_pairs]
     references_to_record = set(flatten_one_layer(reference_locations))
     return module_attr_pairs, references_to_record
+
 
 def record_reference_html(refs_to_record, section_listing):
     references = {get_source_url_from_section_id((page, sect_id)):
