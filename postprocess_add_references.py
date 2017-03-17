8414dbf930f6e6ea40391d6372370c8621f4b603
ReeceStevens <reecestevens24@gmail.com>
2017-03-17 17:59:49 -0500

    Remove side-effect generating dict comp

--- postprocess_add_references.py
+++ postprocess_add_references.py
@@ -1,9 +1,6 @@
 '''
 Find external references in attribute descriptions. When a reference is found,
-add its source URL to the JSON file and record the referenced HTML in the
-`references.json` file, keyed by the source URL. Remove any anchor tag attributes
-on reference links that are successfully recorded.
-'''
+add its source URL to the JSON file and record the referenced HTML in the `references.json` file, keyed by the source URL. Remove any anchor tag attributes on reference links that are successfully recorded.  '''
 import sys
 import re
 from functools import partial
@@ -16,43 +13,16 @@ from macro_utils import flatten_one_layer
 IGNORED_REFERENCES_RE = re.compile(r'(.*ftp.*)|(.*http.*)|(.*part05.*)|(.*chapter.*)|(.*PS3.*)|(.*DCM.*)|(.*glossentry.*)')
 
 
-# TODO: rewrite this so as to not have side effects in a list comprehension
-# a function should either return a computed value from its inputs or modify
-# the input, but almost never both.  In this case, having a function in a list
-# comprehension is quite confusing.  A cursory look at add_refs_to_pairs makes
-# one think that the first return value is unnecessary because it is not being
-# modified anywhere.  It turns out it is NOT necessary, because the value is
-# modified in place via a list comprehension!
-def add_refs_to_pairs(module_attr_pairs):
-    reference_locations = [references_in_module_attr_pair(pair) for pair in module_attr_pairs]
-    references_to_record = set(flatten_one_layer(reference_locations))
-    return module_attr_pairs, references_to_record
+def get_reference_requests_from_pairs(module_attr_pairs):
+    return [references_from_module_attr_pair(pair) for pair in module_attr_pairs]
 
-
-def record_reference_html(refs_to_record, section_listing):
-    references = {get_source_url_from_section_id((page, sect_id)):
-                  resolve_relative_resource_urls(section_listing[page][sect_id])
-                  for page, sect_id in refs_to_record}
-    return references
-
-
-def references_in_module_attr_pair(pair):
+def references_from_module_attr_pair(pair):
     references = get_valid_reference_anchors(pair['description'])
-    reference_locations = list(map(get_ref_standard_location, references))
-    reference_urls = list(map(get_source_url_from_section_id, reference_locations))
-    list(map(mark_as_recorded, references))
-    pair['externalReferences'] = [] if len(reference_urls) < 1 else reference_urls
-    return reference_locations
-
+    return list(map(get_ref_standard_location, references))
 
 def get_valid_reference_anchors(html):
     anchor_tags = BeautifulSoup(html, 'html.parser').find_all('a', href=True)
     return [a for a in anchor_tags if not re.match(IGNORED_REFERENCES_RE, a['href'])]
-
-
-def get_source_url_from_section_id(standard_location):
-    return pl.BASE_DICOM_URL + '#'.join(standard_location)
-
 
 def get_ref_standard_location(reference_anchor_tag):
     relative_link = reference_anchor_tag.get('href')
@@ -60,6 +30,16 @@ def get_ref_standard_location(reference_anchor_tag):
     standard_page = 'part03.html' if standard_page == '' else standard_page
     return standard_page, section_id
 
+
+def find_reference_html_in_sections(refs_from_pairs, section_listing):
+    refs_to_record = set(flatten_one_layer(refs_from_pairs))
+    references = {get_reference_url_from_standard_location((page, sect_id)):
+                  section_listing[page][sect_id]
+                  for page, sect_id in refs_to_record}
+    refs_with_resolved_resource_urls = {k: resolve_relative_resource_urls(v)
+                                        for k, v in references.items()}
+    cleaned_references = {k: pl.clean_html(v) for k, v in refs_with_resolved_resource_urls.items()}
+    return cleaned_references
 
 def resolve_relative_resource_urls(html_string):
     html = BeautifulSoup(html_string, 'html.parser')
@@ -71,9 +51,14 @@ def resolve_relative_resource_urls(html_string):
     list(map(partial(resolve_resource, 'data'), equations))
     return str(html)
 
-
 def resolve_anchor_href(anchor):
     if not has_protocol_prefix(anchor):
+        try:
+            page, fragment_id = anchor['href'].split('#')
+            resolved_page = 'part03.html' if page == '' else page
+            anchor['href'] = resolved_page + '#' + fragment_id
+        except ValueError:
+            pass
         anchor['href'] = pl.BASE_DICOM_URL + anchor['href']
 
 
@@ -85,6 +70,21 @@ def resolve_resource(url_attribute, resource):
     resource[url_attribute] = pl.BASE_DICOM_URL + resource[url_attribute]
 
 
+def record_references_inside_pairs(module_attr_pairs, refs_to_record):
+    updated_pairs = [record_reference_in_pair(pair, refs)
+                     for pair, refs in zip(module_attr_pairs, refs_to_record)]
+    return updated_pairs
+
+def record_reference_in_pair(pair, refs):
+    references = get_valid_reference_anchors(pair['description'])
+    reference_urls = list(map(get_reference_url_from_standard_location, refs))
+    list(map(mark_as_recorded, references))
+    pair['externalReferences'] = [] if len(reference_urls) < 1 else reference_urls
+    return pair
+
+def get_reference_url_from_standard_location(standard_location):
+    return pl.BASE_DICOM_URL + '#'.join(standard_location)
+
 def mark_as_recorded(anchor):
     anchor['href'] = ''
     anchor.name = 'span'
@@ -93,7 +93,8 @@ def mark_as_recorded(anchor):
 if __name__ == '__main__':
     module_attr_pairs = pl.read_json_to_dict(sys.argv[1])
     section_listing = pl.read_json_to_dict(sys.argv[2])
-    module_attr_with_refs, refs_to_record = add_refs_to_pairs(module_attr_pairs)
-    references = record_reference_html(refs_to_record, section_listing)
-    pl.write_pretty_json(sys.argv[3], module_attr_with_refs)
+    refs_to_record = get_reference_requests_from_pairs(module_attr_pairs)
+    references = find_reference_html_in_sections(refs_to_record, section_listing)
+    updated_pairs = record_references_inside_pairs(module_attr_pairs, refs_to_record)
+    pl.write_pretty_json(sys.argv[3], updated_pairs)
     pl.write_pretty_json(sys.argv[4], references)
