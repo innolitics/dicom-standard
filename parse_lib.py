f3b5ab6649baa3b6e79c22bcff0b6676b841190b
Reece Stevens <reecestevens24@gmail.com>
2017-05-17 11:24:31 -0500

    Add docstrings for functions needing context

--- parse_lib.py
+++ parse_lib.py
@@ -35,6 +35,9 @@ def read_json_to_dict(filepath):
 
 
 def all_tdivs_in_chapter(standard, chapter_name):
+    '''
+    Find all HTML tables in a given chapter of the DICOM Standard.
+    '''
     chapter_divs = standard.find_all('div', class_='chapter')
     for chapter in chapter_divs:
         if chapter.div.div.div.h1.a.get('id') == chapter_name:
@@ -48,12 +51,21 @@ def create_slug(title):
 
 
 def find_tdiv_by_id(all_tables, table_id):
+    '''
+    Find a given table tag by its HTML ID
+    '''
     matching_table = (lambda table: pr.table_id(table) == table_id)
     table_with_id = list(filter(matching_table, all_tables))
     return None if table_with_id == [] else table_with_id[0]
 
 
 def clean_table_name(name):
+    '''
+    Remove table name prefixes and suffixes.
+
+    Example:
+        Table C.7-5b. Clinical Trial Series Module Attributes --> Clinical Trial Series
+    '''
     _, _, title = re.split('\u00a0', name)
     possible_table_suffixes = r'(IOD Modules)|(Module Attributes)|(Macro Attributes)|(Module Table)'
     clean_title, *_ = re.split(possible_table_suffixes, title)
@@ -61,6 +73,11 @@ def clean_table_name(name):
 
 
 def clean_html(html):
+    '''
+    Removes unused attributes and empty tags from
+    the HTML. Also updates relative resource URLs
+    to absolute URLs.
+    '''
     parsed_html = BeautifulSoup(html, 'html.parser')
     top_level_tag = get_top_level_tag(parsed_html)
     remove_attributes_from_html_tags(top_level_tag)
@@ -134,6 +151,10 @@ def resolve_href_url(href):
 
 
 def get_short_html_location(reference_link):
+    '''
+    For a given relative URL, generate the link to the short HTML version
+    of the DICOM standard.
+    '''
     standard_page, section_id = reference_link.split('#')
     chapter_with_extension = 'part03.html' if standard_page == '' else standard_page
     chapter, _ = chapter_with_extension.split('.html')
@@ -141,6 +162,9 @@ def get_short_html_location(reference_link):
 
 
 def get_standard_page(sect_id):
+    '''
+    Returns the short HTML page name of the DICOM standard containing `sect_id`.
+    '''
     sections = sect_id.split('.')
     try:
         cutoff_index = sections.index('1')
@@ -170,6 +194,10 @@ def text_from_html_string(html_string):
 
 
 def table_parent_page(table_div):
+    '''
+    Return the short HTML  page name of the DICOM standard containing
+    the specified `table_div`.
+    '''
     parent_section_id = table_div.parent.div.div.div.find('a').get('id')
     sections = parent_section_id.split('.')
     try:
