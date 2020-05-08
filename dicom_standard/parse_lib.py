03003f32cd5b8a52b539e46e1a319a31fddfef3b
Russell Kan <rkan@innolitics.com>
2020-05-08 02:42:53 -0500

    Add unit tests for standard workarounds
    - Update README
    - Issue #19

--- dicom_standard/parse_lib.py
+++ dicom_standard/parse_lib.py
@@ -83,8 +83,8 @@ def clean_table_name(name: str) -> str:
         Table C.7-5b. Clinical Trial Series Module Attributes --> Clinical Trial Series
     '''
     _, _, title = re.split('\u00a0', name)
-    # Standard workaround: Include upper case "S" at end of "IOD Modules" to catch typo in Table A.39.19-1
-    # http://dicom.nema.org/medical/dicom/current/output/chtml/part03/sect_A.35.19.3.html
+    # Standard workaround: Include upper case "S" at end of "IOD Modules" to catch typo in Table A.35.19-1
+    # http://dicom.nema.org/medical/dicom/current/output/chtml/part03/sect_A.35.19.3.html#table_A.35.19-1
     # Standard workaround: Include optional "s" at end of "Functional Group" to catch Table A.32.9-2
     # http://dicom.nema.org/medical/dicom/current/output/chtml/part03/sect_A.32.9.3.4.html#table_A.32.9-2
     possible_table_suffixes = r'(IOD Module[Ss])|(Module Attributes)|((Functional Group)? Macro Attributes)|(Module Table)|(Functional Groups? Macros)'
