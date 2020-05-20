26f0f0344b5dc50310d0ac9d6c231542b9096e66
Russell Kan <rkan@innolitics.com>
2020-05-19 19:54:50 -0500

    Find table descriptions under h5 tags as well as h3 tags
    - Add unit tests
    - Update standard JSON files
    - Issue #38

--- tests/html_snippets.py
+++ tests/html_snippets.py
@@ -121,3 +121,52 @@ bothspan = '''
 </table>
 </div>
 '''
+
+h3_description = '''
+<div class="section">
+<div class="section">
+<div class="titlepage">
+<div>
+<div>
+<h3 class="title">CT Image IOD Description</h3>
+</div>
+</div>
+</div>
+<p>Description</p>
+</div>
+<div class="section">
+<div class="table">
+</div>
+</div>
+</div>
+'''
+
+h5_description = '''
+<div class="section">
+<div class="section">
+<div class="titlepage">
+<div>
+<div>
+<h5 class="title">RT Radiation Set IOD Description</h5>
+</div>
+</div>
+</div>
+<p>Description</p>
+</div>
+<div class="section">
+<div class="table">
+</div>
+</div>
+</div>
+'''
+
+no_description = '''
+<div class="section">
+<div class="section">
+</div>
+<div class="section">
+<div class="table">
+</div>
+</div>
+</div>
+'''
